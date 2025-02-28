f36be53e33f323faa794774eb2a7653e424b1933
Lucas da Silva Pimentel <pimentellucas753@gmail.com>
2025-02-28 16:48:51 -0300

    componente viagem

---
+++ src/models/Veiculo.ts
@@ -0,0 +1,12 @@
+import Viagem from "./Viagem";
+
+export default interface Veiculo {
+    id: number;
+    modelo: string;
+    marca: string;
+    cor: string;
+    placa: string;
+    motorista: string;
+    fotoMotorista: string;
+    viagem?: Viagem[] | null;
+}
