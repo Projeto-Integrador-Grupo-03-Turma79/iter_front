40b7502bb328e420f330f8def53291d83d4682fe
Adelina_Santos <adevitoria.m09@gmail.com>
2025-03-05 09:51:35 -0300

    Inclusões de funções na Home

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 
-export default function Navbar()  {
+export default function Navbar() {
 
     function saida(){
-        alert('Obrigado por usar o nosso sistema!');
+        alert('Obrigado por usar o nosso sistema! Até a próxima viagem!!');
     }
 
     return (
         <>
         <nav className='w-full bg-[#003152] text-white flex justify-center py-4'>
-            <div className="container flex justify-between text-2xl">
-                <div className='flex gap-20 items-center justify-center'>
+            <div className="container flex justify-end text-2xl">
+                <div className='flex gap-20 items-center'>
                     <Link to='/sobre' className='hover:underline'>Sobre Nós</Link>
                     <Link to='/login' className='hover:underline'>Login</Link>
                     <Link to='/cadastro' className='hover:underline'>Cadastre-se</Link>
@@ -18,6 +18,6 @@ export default function Navbar()  {
                 </div>
             </div>
         </nav>
-    </>
+        </>
     )
 }
