100a54206057cf040e65d88dc69cbf29455a2b7f
Otávio Ferreira <pizza131@outlook.com>
2025-03-05 15:07:44 -0300

    Cards Expansíveis

--- src/components/viagens/listarviagens/ListarViagens.tsx
+++ src/components/viagens/listarviagens/ListarViagens.tsx
@@ -41,19 +41,19 @@ function ListaViagens() {
     }, [viagens.length])
 
     return (
-        <>
-            <div className="flex justify-center w-full my-4">
-                <div className="container flex flex-col mx-2">
-                    <div className='container mx-auto my-4 
-                        grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'
-                    >
-                        {viagens.map((viagem) => (
-                            <CardViagens key={viagem.id} viagem={viagem} />
-                        ))}
-                    </div>
+        <div className="flex justify-center w-full my-4">
+            <div className="container flex flex-col mx-2">
+                <div className="container mx-auto my-4 
+                    grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
+                >
+                    {viagens.map((viagem) => (
+                        <div key={viagem.id} className="self-start">
+                            <CardViagens viagem={viagem} />
+                        </div>
+                    ))}
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
