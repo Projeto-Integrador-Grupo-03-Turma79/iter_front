0f3bfc1ddfdf9f9636c9e8aee132a7894f3b9740
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2025-03-15 13:08:15 -0300

    adiconando mapa a home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,22 @@
-import { ChangeEvent, useContext, useState } from "react"
+import { ChangeEvent, Fragment, useContext, useRef, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Usuario from "../../models/Usuario"
 import { AuthContext } from "../../contexts/AuthContext"
 import { ToastAlerta } from "../../utils/ToastAlerta"
-import imagem from "../../assets/Black and Red Dynamic Car Dealer Presentation (1).png";
+import { GoogleMap, useJsApiLoader, DirectionsRenderer, Marker, LoadScript, StandaloneSearchBox } from '@react-google-maps/api'
 
 function Home() {
 
     const navigate = useNavigate()
 
-    const [ usuarios, setUsuarios ] = useState<Usuario>({} as Usuario)
+    const [usuarios, setUsuarios] = useState<Usuario>({} as Usuario)
 
     const { usuario } = useContext(AuthContext)
     const token = usuario.token
+
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: import.meta.env.REACT_GOOGLE_MAP_API_KEY,
+    })
 
     function atualizarEstado(e: ChangeEvent<HTMLInputElement>): void {
         console.log(e.target.value);
@@ -31,18 +35,21 @@ function Home() {
     return (
         <>
             <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
+
                 <form className="flex justify-center items-center flex-col w-1/2 gap-4">
                     <h2 className="text-slate-900 text-5xl">Inicie a Sua Viagem</h2>
                     <div className="flex flex-col w-full">
                         <label htmlFor="partida">Partida:</label>
-                        <input
-                            type="text"
-                            id="partida"
-                            name="partida"
-                            placeholder="Partida"
-                            className="border-2 border-slate-700 rounded-4xl p-2"
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                        />
+                        <div className="flex flex-col w-full ">
+                            <input
+                                type="text"
+                                id="partida"
+                                name="partida"
+                                placeholder="Partida"
+                                className="border-2 border-slate-700 rounded-4xl p-2"
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                            />
+                        </div>
                     </div>
                     <div className="flex flex-col w-full ">
                         <label htmlFor="destino">Destino:</label>
@@ -55,8 +62,8 @@ function Home() {
                         />
                     </div>
                     <button
-                        type="button" 
-                        onClick={irParaLogin} 
+                        type="button"
+                        onClick={irParaLogin}
                         className="rounded-4xl 
                         bg-black  flex justify-center hover:bg-slate-800 text-white w-1/2 py-2"
                     >
@@ -65,16 +72,26 @@ function Home() {
                     <hr className="border-slate-800 w-full" />
                 </form>
 
-                <div className="flex justify-center">
-                    <img
-                        src={imagem}
-                        alt="Imagem Página Home"
-                        className="aspect-square object-cover w-175 rounded-4xl shadow-xl"
-                    />
+                <div style={{ width: "100%", height: "85%" }}>
+                    {
+                        isLoaded ? (
+                            <GoogleMap
+                                mapContainerStyle={{ width: "94%", height: "85%" }}
+                                center={{
+                                    lat: -23.56535674962746,
+                                    lng: -46.69639212509296
+                                }}
+                                zoom={15}>
+                            </GoogleMap>
+                        ) : null}
                 </div>
+
+
+
             </div>
         </>
     );
 }
+
 
 export default Home;
